634af2a562ad12bb5ec2766cb9082726b5ac0a5c
Enock Cheruiyot <40239524+EnockBrown@users.noreply.github.com>
2020-02-29 17:45:26 +0300

    Update RetrofitClient.java

--- app/src/main/java/com/example/studentapp/api/RetrofitClient.java
+++ app/src/main/java/com/example/studentapp/api/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    public static final String BASE_URL="http://ipaddress/retrofit/public/";
+    public static final String BASE_URL="http://Your Ip Address/retrofit/public/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
